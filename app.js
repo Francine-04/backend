1236f6b479726173262778bcfcdd32da44b8f0bf
Francine-04 <fdelatorre@gbox.ncf.edu.ph>
2024-12-13 20:44:16 +0800

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 require("dotenv").config();  
 const express = require("express");  
 const cors = require("cors");  
-const bodyParser = require("body-parser"); /
-const db = require("./db"); 
 const userRoutes = require("./routes/userRoutes");  
 const postRoutes = require("./routes/postRoutes");  
 
 const app = express();  
 
 // Middleware  
-app.use(cors()); // Enable CORS for all routes  
-// Using Express's built-in JSON middleware instead of body-parser  
-app.use(express.json());   
+app.use(cors());  
+app.use(express.json());  
 
 // Routes  
 app.use("/api/users", userRoutes);  
