7fc69af1dc7178e559dc881a1d170b0392a718bd
Francine-04 <fdelatorre@gbox.ncf.edu.ph>
2024-12-15 20:55:11 +0800

    first commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173", // Frontend origin
+  origin: "https://backend-n41a.onrender.com", // Frontend origin
   credentials: true,
 }));
 
