02e05a5b3c4b006c1876e19bd70dfab50ad63559
Francine-04 <fdelatorre@gbox.ncf.edu.ph>
2024-12-15 17:59:38 +0800

    first commit

--- app.js
+++ app.js
@@ -12,12 +12,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: 'https://backend-n41a.onrender.com',  // Adjust according to your frontend environment
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
 // Middleware
 app.use(cors());
 app.use(express.json());
